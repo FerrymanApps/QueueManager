181d74a3017cc095750cda3640db2be1c41ead77
Jeffrey Powershell Lee <jeffrey.lee@ferrymanapps.com>
2016-04-03 00:36:12 -0400

    the function successfully sends a message to sqs

--- queueManager.py
+++ queueManager.py
@@ -1,6 +1,6 @@
 from __future__ import print_function
 
-import json
+import json,boto3
 
 def queueManager_handler(event, context):
     messageBody = {
@@ -9,9 +9,25 @@ def queueManager_handler(event, context):
         'password': event['password']
         }
 
-    print("action =" + event['action'])
-    print("username =" + event['username'])
-    print("password =" + event['password'])
+    # Get the service resource
+    sqs = boto3.resource('sqs')
+
+    # Find the queue. This returns an SQS.Queue instance
+    queue = sqs.get_queue_by_name(QueueName='TestQueue')
+
+    # access identifiers and attributes
+    print("SQS URL = " + queue.url)
+
+    # Create a new message
+    response = queue.send_message(MessageBody=json.dumps(messageBody))
+
+    # The response is NOT a resource, but gives you a message ID and MD5
+    print("SQS Message ID = " + response.get('MessageId'))
+    print("SQS Message MD5 = " + response.get('MD5OfMessageBody'))
+
+    #print("action =" + event['action'])
+    #print("username =" + event['username'])
+    #print("password =" + event['password'])
 
     message = 'The following message has been queued in successfully: {}'.format(json.dumps(messageBody))
     
